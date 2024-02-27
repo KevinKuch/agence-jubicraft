c899aebcabdbe41db586158794db07146368fb1c
Kevin Kuch <kuchkevin96@gmail.com>
2024-02-27 00:12:05 -0500

    Section 2 image scaling

--- app/(landing)/_components/section2.tsx
+++ app/(landing)/_components/section2.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import styles from './style.module.scss'
+import Image from 'next/image'
+import Image1 from '../../../public/image1.jpg'
+import { useRef } from 'react'
+import { useScroll, useTransform, motion } from 'framer-motion'
+
+
+
+
 const SectionImages = () => {
+
+	const container = useRef(null)
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ['start start', 'end end']
+	})
+
+	// L'image est de 25vw et 25vh, donc on le multiplie par 4 pour qu'elle prenne toute la hauteur de l'écran
+	const scaleCenterImage = useTransform(scrollYProgress, [0, 1], [1, 4])
+
 	return ( 
-		<div className="h-screen bg-red-800">
-			Section images
+		<div ref={container} className={`${styles.container} h-300 bg-red-800`}>
+			<div className={styles.sticky}>
+				<div className={styles.element}>
+					<motion.div style={{scale: scaleCenterImage}} className={styles.imageContainer}>
+						<Image
+							src={Image1}
+							fill
+							alt='image1'
+							placeholder='blur'
+						/>
+					</motion.div>
+				</div>
+			</div>
 		</div>
 	 );
 }
