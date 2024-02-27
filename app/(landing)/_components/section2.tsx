113eddcee0a2644b7a04b3619d07ca5b7d64b809
Kevin Kuch <kuchkevin96@gmail.com>
2024-02-27 00:49:00 -0500

    Ajout bg gradient

--- app/(landing)/_components/section2.tsx
+++ app/(landing)/_components/section2.tsx
@@ -12,12 +12,17 @@ import { useScroll, useTransform, motion } from 'framer-motion'
 const SectionImages = () => {
 
 	const container = useRef(null)
+	// On utilise useScroll (framer-motion) pour observer le défilement de la page
 	const { scrollYProgress } = useScroll({
+		// Spécification de la cible à observer pour le défilement
 		target: container,
+		// Définition des offsets pour le début et la fin du défilement
 		offset: ['start start', 'end end']
 	})
 
 	// L'image est de 25vw et 25vh, donc on le multiplie par 4 pour qu'elle prenne toute la hauteur de l'écran
+	// [0, 1] est la plage de défilement
+	// [1, 4] est la plage de transformation en scale
 	const scaleCenterImage = useTransform(scrollYProgress, [0, 1], [1, 4])
 
 	return ( 
