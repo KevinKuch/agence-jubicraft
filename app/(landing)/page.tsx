1b9bf5a40e0deacedc8a601e870bf571289b6ed4
Kevin Kuch <kuchkevin96@gmail.com>
2024-02-29 01:43:39 -0500

    Remove margin and add overflow hidden

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
 			<div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10 mb-40">
 				<Hero />
 			</div>
-			{/* <Section2 /> */}
+			<Section2 />
 			<Section3 />
 		</div>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import LandingPage from "./(landing)/page";
 
 export default function Home() {
 	return (
-		<main className="ml-6 mr-6 border border-red-600">
+		<main className="border border-red-600">
 			<LandingPage />
 		</main>
 	)
