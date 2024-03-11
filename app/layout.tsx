74dfdafb2842ece51f76dad17b68882978d18c4b
Kevin Kuch <kuchkevin96@gmail.com>
2024-03-10 23:24:31 -0400

    update next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Telex } from "@next/font/google";
+import { Telex } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
