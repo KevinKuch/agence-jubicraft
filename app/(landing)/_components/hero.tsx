4affb6c26a2adbc6abb4d91d6f91eb902bd468fa
Kevin Kuch <kuchkevin96@gmail.com>
2024-03-11 23:00:53 -0400

    place img

--- app/(landing)/_components/hero.tsx
+++ app/(landing)/_components/hero.tsx
@@ -22,57 +22,41 @@ export const Hero = () => {
 		visible: { opacity: 1, y: 0 }
 	};
 
-	// useGSAP est un hook qui permet d'appliquer des animations et cela tue automotiquement les animations sans avoir à utiliser useEffect avec sa dépendance
-	// useGSAP(() => {
-	// 	const tl = gsap.timeline();
-
-	// 	tl.from(".heroText", {
-	// 		y:200,
-	// 		ease: "power4.out",
-	// 		duration: 0.8,
-	// 		stagger: {
-	// 			amount: 0.4,
-	// 		}
-	// 	})
-
-	// 	tl.to("#dropedText", {
-	// 		transformOrigin: "top right",
-	// 		rotation: -2,
-	// 		duration:1,
-	// 		ease: "bounce.out",
-	// 	})
-	// })
-
 	return (
-    <div className={`${styles.main} min-h-screen w-full space-y-4 pt-[8rem] relative`}>
-        <div className="z-10 flex flex-col justify-center items-center pt-2 lg:mx-12">
-            <div className="overflow-hidden">
-                <h2 className={`${styles.secondText} heroText text-9xl uppercase font-heebo font-black `}>
-                    Créativité,
-                </h2>
-            </div>
-            <div className="overflow-hidden">
-                <h1 id="dropedText" className={`${styles.bigText} heroText text-[10rem] font-bold uppercase font-rubik-doodle p-6 `}>
-                    Visionnement
-                </h1>
-            </div>
-            <div className="overflow-hidden">
-                <h2 className={`${styles.secondText} heroText uppercase text-8xl font-heebo font-black `}>
-                    Impéccable
-                </h2>
-            </div>
-            <h3 className={`${styles.subText} font-bold pt-[8rem]`}>
-                Agence de création évènementielle
-            </h3>
+    <div className={`${styles.main} min-h-screen w-full pt-[8rem] relative`}>
+        <div className="flex flex-col justify-center pt-2 pl-12 lg:mx-12">
+					<div className="text-container relative">
+						<div className="overflow-hidden">
+								<h2 className={`${styles.secondText} uppercase font-heebo font-black`}>
+										Créativité,
+								</h2>
+						</div>
+						<div className="overflow-hidden">
+								<h1 className={`${styles.secondText} font-bold uppercase font-heebo `}>
+										Visionnement
+								</h1>
+						</div>
+						<div className="overflow-hidden">
+								<h2 className={`${styles.bigText} uppercase font-rubik-doodle font-black `}>
+										Impéccable
+								</h2>
+						</div>
+					</div>
+					<h3 className={`${styles.subText} font-bold pt-[3rem]`}>
+							Agence de création évènementielle
+					</h3>
+					<div className="absolute bottom-0 left-20 sm:top-[2rem] sm:left-[23rem] lg:top-[-50px] lg:left-[34rem] max-w-[1200px]">
+						<iframe className="mx-auto w-[60vw] h-[60vh] lg:w-[80vw] lg:h-[85vh]" src="https://lottie.host/embed/39972fb0-b576-488c-89e9-62426c91722a/VpRm4oesTK.json"></iframe>
+					</div>
         </div>
-				<div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 pb-6 flex justify-around items-center gap-12 w-full max-w-[1500px]">
-    <div className="flex-none relative w-full max-w-[200px] aspect-[70/45]">
-        <Image className="w-52" src="/jc-logo-initial.svg" alt="Jubilee Craft logo" fill />
-    </div>
-    <p className={`${styles.placeText} uppercase`}>Montreal, Toronto et Quebec</p>
-</div>
 
-    </div>
+				<div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 pb-6 flex justify-around items-center gap-12 w-full max-w-[1500px]">
+				<div className="flex-none relative w-full max-w-[200px] aspect-[70/45]">
+						<Image className="w-52" src="/jc-logo-initial.svg" alt="Jubilee Craft logo" fill />
+				</div>
+					<p className={`${styles.placeText} uppercase`}>Montreal, Toronto et Quebec</p>
+				</div>
+  </div>
 );
 
 };
--- app/(landing)/_components/sectionAbout.tsx
+++ app/(landing)/_components/sectionAbout.tsx
@@ -13,8 +13,6 @@ import ImageAbout2 from '@/public/image7.jpg';
 
 const About = () => {
 
-	const textes = ["Chez Jubilee Craft, votre succès est la nôtre", "créativité débordante et expertise", "Nous mettons en œuvre notre intelligence créative collective à chaque projet pour atteindre cet objectif commun", "Vivez une expériences mémorables"]
-
 	const phrase = "Chez Jubilee Craft, nous somme reconnus pour notre grande sensibilité à percevoir et à créer la beauté en nous réinventant constamment."
 	
 	const text = useRef(null);
@@ -25,15 +23,15 @@ const About = () => {
 			// effet voulue
 				scrollTrigger: {
 						trigger: '.about-split-text', // déclencheur
-						start: 'top center',
+						start: 'bottom center',
 						end: 'bottom center',
 						scrub: true, // permet de faire l'animation en même temps que le scroll
-						// markers: true,
+						// markers: true, 
 				},
 				// effet départ
 				opacity: 0.2,
 				stagger: 0.1,
-				duration: 2,
+				duration: 6,
 		});
 });
 
@@ -42,12 +40,12 @@ const About = () => {
 	// Variante de l'animation pour le texte Revealing
 	const slideUpText = {
 		initial: {y: "100%"},
-		animate: (i: number) => ({y: 0 , transition: {duration: 0.4, delay: i * 0.04}, delay:5}),
+		animate: (i: number) => ({y: 0 , transition: {duration: 0.8, delay: i * 0.04}, delay:5}),
 		hidden: {y: "100%"}
 	}
 
 	return ( 
-		<section ref={container} className={`${styles.about} h-[200vh] bg-[#613870] overflow-hidden`}>
+		<section ref={container} className={`${styles.about} h-[130vh] lg:h-[200vh] bg-[#613870] overflow-hidden`}>
 			<div className='text-white font-trirong font-black text-4xl lg:text-7xl mx-auto my-16 max-w-[1200px]'>
 				<h1 className='relative inline-flex overflow-hidden'>
 					<motion.span variants={slideUpText} initial="initial" animate={isInView ? "animate" : "hidden"}  className=''>Artisans, Esthétiques</motion.span>
@@ -57,7 +55,7 @@ const About = () => {
 				</h1>
 			</div>
 			<div className='text-white font-telex md:text-xl lg:text-3xl md:mx-auto md:max-w-[1200px]'>
-				<p ref={text} className="pb-6 lg:pb-16 lg:pl-[35rem]">
+				<p ref={text} className="pb-6 lg:pb-16 lg:pl-[26rem]">
 					{
 						// Fonction JS pour séparer les mots et les animer. Chaque mot est un span
 						phrase.split(" ").map((word, index) => {
@@ -66,14 +64,14 @@ const About = () => {
 					}
 				</p>
 				<div>
-					<h2 data-scroll data-scroll-speed="0.1" className='pb-16 lg:pl-[35rem]'>À propos <iframe className="w-20 h-20" src="https://lottie.host/embed/c2cb0509-6959-4fad-8882-1ba69c93dfe3/EReLfnb8vo.json"></iframe></h2>
+					<h2 data-scroll data-scroll-speed="0.1" className='pt-14 lg:pl-[35rem]'>À propos <iframe className="w-20 h-20" src="https://lottie.host/embed/c2cb0509-6959-4fad-8882-1ba69c93dfe3/EReLfnb8vo.json"></iframe></h2>
 				</div>
 			</div>
 			<div className={`${styles.imgContainer}`}>
-				<div data-scroll data-scroll-speed="0.2" className={`${styles.imgElement}`}>
+				<div data-scroll data-scroll-speed="0.1" className={`${styles.imgElement}`}>
 					<Image src={ImageAbout} fill alt="image-about" />
 				</div>
-				<div data-scroll data-scroll-speed="0.3" className={`${styles.imgElement} `}>
+				<div data-scroll data-scroll-speed="0.1" className={`${styles.imgElement} `}>
 					<Image src={ImageAbout2} fill alt="image-about" />
 				</div>
 			</div>
